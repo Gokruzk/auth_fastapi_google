00ab80ee41235fc196cb46db184e47fd11c533c8
gokruzk <nigelljama@gmail.com>
2025-08-11 16:19:18 -0500

    get cookie

--- frontend/src/app/profile/page.tsx
+++ frontend/src/app/profile/page.tsx
@@ -1,26 +1,26 @@
-"use client"
+import { cookies } from "next/headers";
+// import { authUser } from "@/api";
 
-import { authUser } from "@/api";
-import { useEffect, useState } from "react";
-
-export default function Profile() {
-  const [currentUser, setCurrentUser] = useState<string | undefined>();
-
+export default async function Profile() {
+  const cookieStore = await cookies();
+  const access_token = cookieStore.get("access_token");
   // get current user
-  useEffect(() => {
-    const fetchUserSession = async () => {
-      const { data } = await authUser();
-      console.log(data);
-      setCurrentUser("Nombre de usuario");
-    };
+  // useEffect(() => {
+  //   const fetchUserSession = async () => {
+  //     const { data } = await authUser();
+  //     console.log(data);
+  //     setCurrentUser("Nombre de usuario");
+  //   };
 
-    fetchUserSession();
-  }, []);
+  //   fetchUserSession();
+  // }, []);
+
+  console.log(access_token);
 
   return (
     <main>
       <div className="px-9 pt-5">Google Auth</div>
-      <div className="px-9 py-5">Nombre: {currentUser}</div>
+      <div className="px-9 py-5">Nombre:</div>
     </main>
   );
 }
