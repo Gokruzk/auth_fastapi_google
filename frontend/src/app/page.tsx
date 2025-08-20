04b0e33ccd5fa609b3ef24f7b5a29f837d8b3536
gokruzk <nigelljama@gmail.com>
2025-08-20 18:49:20 -0500

    remove console.log

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,30 +1,20 @@
 "use client";
 
-import { useEffect, useState, useRef } from "react";
+import { useEffect, useState } from "react";
 
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
-// const FRONTEND_URL = process.env.NEXT_PUBLIC_FRONT_URL;
-// const allowedOrigins = [`${FRONTEND_URL}`, `${API_URL}`];
 
 export default function Home() {
   const [token, setToken] = useState<string | null>(null);
-  const popupRef = useRef<Window | null>(null);
 
   useEffect(() => {
     function handleMessage(event: MessageEvent) {
-      // if (!allowedOrigins.includes(event.origin)) return;
-
-      console.log("Evento recibido:", event.origin, event.data);
-
+      if (event.origin !== API_URL) return;
       const { token } = event.data;
       if (token) {
         setToken(token);
         localStorage.setItem("access_token", token);
-
-        // Cierra la ventana emergente si sigue abierta
-        if (popupRef.current && !popupRef.current.closed) {
-          popupRef.current.close();
-        }
+        // -> /auth
       }
     }
 
@@ -34,6 +24,7 @@ export default function Home() {
       window.removeEventListener("message", handleMessage);
     };
   }, []);
+
   function openLoginPopup() {
     const width = 500;
     const height = 600;
